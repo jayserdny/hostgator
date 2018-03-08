ae6213bab1920ec6cc8703c6b086160dd339705a
Peter Rowny <Peter@Rowny.com>
2018-03-08 11:01:50 -0500

    Change TipDocument Name displayname to 'Title' in Index file also.

--- MVC5-Seneca/EntityModels/TipDocument.cs
+++ MVC5-Seneca/EntityModels/TipDocument.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Web.Mvc;
 using Newtonsoft.Json;
 namespace MVC5_Seneca.EntityModels
@@ -8,7 +9,10 @@ namespace MVC5_Seneca.EntityModels
     {
         [JsonProperty] public int Id { get; set; }
         [JsonProperty] public virtual TipsCategory Category { get; set; }
+
+        [DisplayName("Title")]
         [JsonProperty] public string Name { get; set; }
+
         [JsonProperty] public string DocumentLink { get; set; }                           
         [JsonProperty] public virtual ApplicationUser User { get; set; }    // who submitted this tip
 
