20433e88e0ad9c9de2a1832166b8cacf677a8687
Peter Rowny <Peter@Rowny.com>
2018-03-11 12:34:49 -0400

    create Login entity model

---
+++ MVC5-Seneca/EntityModels/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MVC5_Seneca.EntityModels
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Status { get; set; }
+    }
+}
