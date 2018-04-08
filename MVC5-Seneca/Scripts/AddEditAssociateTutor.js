9717ac25da28ab6eaff8365dbcc55545c4595197
Peter Rowny <Peter@Rowny.com>
2018-04-08 15:23:30 -0400

    "Pending Add" file

--- MVC5-Seneca/Scripts/AddEditAssociateTutor.js
+++ MVC5-Seneca/Scripts/AddEditAssociateTutor.js
@@ -1,4 +1,14 @@
 ﻿function UpdateStudentDropdownList()
-{
-    var dummy = "";
+{                             
+    var tutorId = $(this).val();
+    $.ajax({
+        url: "/AssociateTutors/GetStudents",
+        data: { id: tutorId }, type: "GET", dataType: "JSON",
+        success: function (data) {
+            $("#studentsDDL").append('<option value = >' + "-Select Student-" + '</option > ');
+            $.each(data.Students, function (i, student) {
+                $("#studentsDDL").append('<option value = "' + '">' + student.FirstName + '</option>');
+            }
+        }
+      }); // $.ajax({ 
 }
